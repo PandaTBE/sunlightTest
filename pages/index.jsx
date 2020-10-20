ae9f18d05d41883a2fb21aa83f00a9fc1cb005ff
Nikita <surov.n2019@gmail.com>
2020-10-20 20:56:27 +0300

    fix error

--- pages/index.jsx
+++ pages/index.jsx
@@ -177,8 +177,8 @@ export default function MainLayout() {
                     <path d="M0 0H1481V470C1114.99 -38.4972 367.553 634.454 0 401.971V0Z" fill="url(#paint0_linear)"/>
                     <defs>
                         <linearGradient id="paint0_linear" x1="1481" y1="235.001" x2="-15.6763" y2="235.001" gradientUnits="userSpaceOnUse">
-                        <stop stop-color="#2196F3"/>
-                        <stop offset="1" stop-color="#1EC3AF"/>
+                        <stop stopColor="#2196F3"/>
+                        <stop offset="1" stopColor="#1EC3AF"/>
                         </linearGradient>
                     </defs>
                 </svg>
